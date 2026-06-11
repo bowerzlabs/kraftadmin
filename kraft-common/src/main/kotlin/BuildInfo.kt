b2afc0927d8df78158669b2c7cbe9402be6c9869
Nyadero Odhiambo <briannyadero443@gmail.com>
2026-06-11 18:00:04 +0300

    chore: bump version

--- kraft-common/src/main/kotlin/BuildInfo.kt
+++ kraft-common/src/main/kotlin/BuildInfo.kt
@@ -1,7 +1,7 @@
 package com.kraftadmin
 
 object BuildInfo {
-    const val VERSION = "0.1.9-beta"
+    const val VERSION = "0.1.14-beta"
     const val GROUP = "com.bowerzlabs"
     const val ARTIFACT_ID = "kraft-admin"
 }
